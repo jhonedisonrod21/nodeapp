aaf51536aa8c037d80afdbef499f9690ab847d6d
jhonedisonrod21 <jhonedisonrod21@gmail.com>
2020-07-21 21:23:54 -0500

    eqweqwe

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
- nohup npm install >> /dev/null && node ./server/server.js >> /dev/null & 
+ echo 'instalando dependencias'
+ nohup npm install >> /dev/null & 
+ sleep 3s
+ echo 'iniciando el servicio'
+ nohup node ./server/server.js >> /dev/null & 
